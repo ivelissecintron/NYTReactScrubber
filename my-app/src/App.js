d4977e036ed49b14e3bf0f216b450934aa5b8709
Ivelisse <msivelisse@gmail.com>
2018-08-07 16:25:41 -0500

    created react app and components

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">New York Times React Scrubber</h1>
+          <h2 className="App-intro">Search for and annotate articles of interest!</h2>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
